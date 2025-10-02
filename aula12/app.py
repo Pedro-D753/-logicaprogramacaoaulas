492981c8540bc4dafe96318dca9256d1dc77741a
Pedro Domiense dos Santos <pedro.domiense05@gmail.com>
2025-10-02 08:16:52 -0300

    crud com 2 classes

--- aula12/app.py
+++ aula12/app.py
@@ -29,13 +29,13 @@ def salvar():
 
 class Livro:
     def __init__(self, id_livro: int, titulo: str, autor: str, ano: int):
-        self.__id_livro = id_livro
+        self.__id_in = id_livro
         self.__titulo = titulo
         self.__autor = autor
         self.__ano = ano
 
     def __str__(self):
-        return f"[{self.ID}] {self.nome_livro} — {self.autor}"
+        return f"[{self.__id_in}] {self.__titulo} {self.__ano} — {self.__autor}"
 
 while True:
     print('\n--- SISTEMA DE GERENCIAMENTO DE BIBLIOTECA ---')
@@ -57,15 +57,16 @@ while True:
         nome = input('Digite o nome do livro: ').strip()
         autor = input('Digite o nome do autor do livro: ').strip()
         id_in = input('Digite o número do ID: ').strip()
+        ano = input('Digite o ano que o livro foi publicado: ')
 
         # checagem simples de ID duplicado 
         if any(str(l.get('ID')) == id_in for l in livros):
             print("ID já existe. Escolha outro ou atualize o livro existente.")
         else:
-            livro = {'nome': nome, 'autor': autor, 'ID': id_in}
+            livro = {'nome': nome, 'autor': autor, 'ID': id_in, 'ano': ano}
             livros.append(livro)
             salvar()
-            print("✅ Livro cadastrado!")
+            print("Livro cadastrado!")
 
     elif opcao == '2':  # listar
         if not livros:
@@ -74,8 +75,9 @@ while True:
             for l in livros:
                 nome = l.get('nome', '')
                 autor = l.get('autor', '')
-                id_ = l.get('ID', '')
-                dados = Livro(nome, autor, id_)
+                id_in = l.get('ID', '')
+                ano = l.get('ano', '')
+                dados = Livro(nome, autor, id_in, ano)
                 print(dados)
 
     elif opcao == '3':  # atualizar
@@ -85,8 +87,10 @@ while True:
             if str(l.get('ID')) == id_alvo:
                 novo_nome = input(f"Novo nome ({l.get('nome')}): ").strip() or l.get('nome')
                 novo_autor = input(f"Novo autor ({l.get('autor')}): ").strip() or l.get('autor')
+                novo_ano = input(f"Novo ano ({l.get('ano')}): ").strip() or l.get('ano')
                 livros[i]['nome'] = novo_nome
                 livros[i]['autor'] = novo_autor
+                livros[i]['ano'] = novo_ano
                 salvar()
                 print("Livro atualizado!")
                 encontrado = True
